c8d41d1d05f6acf059b7e9fc29c989bd7ac943bf
Hack Reactor Students <accounts@hackreactor.com>
2015-11-04 12:06:22 -0800

    Revert "Set production environment variables"
    This reverts commit 15e6fc64083b57ded4347e42ef32f48d83cb8216.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var app = require('./server-config.js');
 
-var port = process.env.PORT || 4568;
-var host = process.env.HOST || '127.0.0.1';
+var port = 4568;
 
-app.listen(port, host);
+app.listen(port);
 
 console.log('Server now listening on port ' + port);
